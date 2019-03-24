a1630cdccb7a7981b00bce1b4e9bd45fc511afc5
Artemis <artemis-kalemis@hotmail.com>
2019-03-24 11:39:10 +0100

    Update: cleaner code with linter

--- routes.js
+++ routes.js
@@ -1,97 +1,89 @@
+/* eslint-disable new-cap */
+/* eslint-disable require-jsdoc */
 
-var slug = require('slug')
-var find = require('array-find')
-var mongo = require('mongodb')
-var session = require('express-session')
+const mongo = require('mongodb');
 
 // setting database
-require('dotenv').config()
+require('dotenv').config();
 
-var db = null
-var url = 'mongodb://' + process.env.DB_HOST + ':' + process.env.DB_PORT
+let db = null;
+const url = 'mongodb://' + process.env.DB_HOST + ':' + process.env.DB_PORT;
 
-mongo.MongoClient.connect(url, { useNewUrlParser: true }, function (err, client) {
-  if (err) throw err
-  db = client.db(process.env.DB_NAME)
-})
+mongo.MongoClient.connect(url, {useNewUrlParser: true}, function(err, client) {
+  if (err) throw err;
+  db = client.db(process.env.DB_NAME);
+});
 
 exports.home = function(req, res, next) {
-    db.collection('profile').find().toArray(done)
-  
-    function done(err, data) {
-        res.render('home.ejs', {data: data})
-      }
+  db.collection('profile').find().toArray(done);
+
+  function done(err, data) {
+    res.render('home.ejs', {data: data});
   }
+};
 
 exports.profile = function(req, res, next) {
-    var id = req.params.id
+  const id = req.params.id;
 
-    db.collection('profile').findOne({
-        _id: mongo.ObjectId(id)
-    }, done)
-
-    function done(err, data) {
-        if (err) {
-        next(err)
-        } else {
-        res.render('profile.ejs', {data: data})
-        }
-    }
-}
-
-exports.form = function(req, res) {   
-    db.collection('profile').insertOne({
-      email: req.body.email,
-      password: req.body.password,
-      profile: req.file ? req.file.filename : null,
-      firstname: req.body.firstname,
-      lastname: req.body.lastname,
-      age: req.body.age
-    }, done)
-  
-    function done(err, data) {
-      if (err) {
-        next(err)
-      } else {
-        res.redirect('/' + data.insertedId)
-      }
-    }
-}
-
-exports.loginForm = function(req, res, next) {   
+  db.collection('profile').findOne({
+    _id: mongo.ObjectId(id),
+  }, done);
 
   function done(err, data) {
     if (err) {
-      next(err)
+      next(err);
     } else {
-      res.redirect('/')
+      res.render('profile.ejs', {data: data});
     }
   }
-}
+};
+
+exports.form = function(req, res) {
+  db.collection('profile').insertOne({
+    email: req.body.email,
+    password: req.body.password,
+    profile: req.file ? req.file.filename : null,
+    firstname: req.body.firstname,
+    lastname: req.body.lastname,
+    age: req.body.age,
+  }, done);
+
+  function done(err, data) {
+    if (err) {
+      next(err);
+    } else {
+      res.redirect('/' + data.insertedId);
+    }
+  }
+};
+
+exports.loginForm = function(req, res, next) {
+  res.redirect('/');
+};
 
 exports.register = function(req, res) {
-    res.render('register.ejs')
-}
+  res.render('register.ejs');
+};
 
 exports.login = function(req, res) {
-  res.render('login.ejs')
-}
+  res.render('login.ejs');
+};
 
 exports.member = function(req, res) {
-    res.render('member.ejs', {data: data})
-}
+  res.render('member.ejs', {data: data});
+};
 
 exports.remove = function(req, res, next) {
-    var id = req.params.id;
-    db.collection("profile").deleteOne({
-        _id: mongo.ObjectID(id)
-    }, done);
-    
-    function done(err) {
-        if (err) {
-            next(err);
-        } else {
-            res.json({status: "ok"})
-        }
+  const id = req.params.id;
+  db.collection('profile').deleteOne({
+    _id: mongo.ObjectId(id),
+  }, done);
+
+  function done(err) {
+    if (err) {
+      next(err);
+    } else {
+      res.json({status: 'ok'});
     }
-}
+  }
+};
